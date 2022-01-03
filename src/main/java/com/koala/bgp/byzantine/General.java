82440cdb8a349d2858f36a576846e8ef9dd203fd
[tomtar00] <[33207@student.pwsztar.edu.pl]>
2022-01-03 19:11:50 +0100

    hotfixes

--- src/main/java/com/koala/bgp/byzantine/General.java
+++ src/main/java/com/koala/bgp/byzantine/General.java
@@ -219,28 +219,20 @@ public class General extends BlockchainNode implements Drawable
                     }
                     else if (algorithm == "Voter" || algorithm == "q-Voter") {
                         if (currentRound == 1) {
-                            String log = getName() + " === ";
                             // select q-sized group of the nearest neighbors
-                            //ArrayList<Integer> mins = new ArrayList<>();
                             for (int i = 0; i < group.length; i++) {
                                 int minIndex = (responseTimes[0] == null) ? 1 : 0;
                                 for (int j = 0; j < responseTimes.length; j++) {
-
                                     if (responseTimes[j] == null)
                                         continue;
-
-                                    if (responseTimes[j] < responseTimes[minIndex] /* && !mins.contains(j) */) {
+                                    if (responseTimes[j] < responseTimes[minIndex]) {
                                         minIndex = j;
                                     }
-
                                 }
 
-                                //mins.add(minIndex);
                                 responseTimes[minIndex] = null;
                                 group[i] = CommandService.getGenerals().get(minIndex);
-                                log += group[i].getName() + ", ";
                             }
-                            SimpleLogger.print(log);
                         }
                     }
 
